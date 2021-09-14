127f175edfce30e2fb0c410b77132e924040c744
Hemanth <hemanthhari2000@gmail.com>
2021-09-14 12:11:10 +0530

    [Hemanth] Create React Base App

---
+++ cs_site_react/src/App.js
@@ -0,0 +1,12 @@
+// import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./screens/HomePage/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ cs_site_react/src/screens/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page Content</h1>
+        </div>
+    )
+}
+
+export default HomePage
