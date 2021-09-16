bba528c04a822750b097c3c5d46b2e39de3a3672
Hilda24 <godlin.hilda@gmail.com>
2021-09-16 08:10:34 +0530

    Modified Footer

--- cs_site_react/src/components/footer.jsx
+++ cs_site_react/src/components/footer.jsx
@@ -1,34 +1,36 @@
 import React from 'react'
+import background from "../images/Footer.svg"
+import 'font-awesome/css/font-awesome.min.css';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="row m-auto p-5">
-                <div className="col-4">
+        <div className="footer d-flex container-fluid" style={{ backgroundImage: `url(${background})` }}>
+            <div className="row mt-3 pt-5 footer-contents">
+                <div className="col-3">
                     <span className="fw-bold"> ABOUT</span>
-                    <span className="d-block mt-3 details"> Faculty</span>
-                    <span className="d-block details"> Mentor</span>
-                    <span className="d-block details"> Student Welfare Committee</span>
+                    <span className="d-block mt-3 mb-2 details"> Faculty</span>
+                    <span className="d-block mb-2 details"> Mentor</span>
+                    <span className="d-block mb-2 details"> Student Welfare Committee</span>
                 </div>
-                <div className="col-4">
+                <div className="col-3">
                     <span className="fw-bold"> RESOURCES</span>
-                    <span className="d-block mt-3 details">News letter</span>
-                    <span className="d-block details">Magazine</span>
-                    <span className="d-block details">IEEE & CSI</span>
+                    <span className="d-block mt-3 mb-2 details">News letter</span>
+                    <span className="d-block mb-2 details">Magazine</span>
+                    <span className="d-block mb-2 details">IEEE & CSI</span>
                 </div>
                 <div className="col-4">
                     <span className="fw-bold"> CONTACT INFORMATION</span>
-                    <div className="row mt-3">
+                    <div className="row mt-3 mb-2">
                         <div className="col-1">
-                            <i className="fas fa-map-marker-alt"></i>
+                            <i className="fa fa-map-marker"></i>
                         </div>
                         <div className="col-11">
-                            <span className=""> St. Joseph's College Of Engineering<br /> OMR <br />Chennai</span>
+                            <span className=""> St. Joseph's College Of Engineering<br /> OMR Chennai</span>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row mb-2">
                         <div className="col-1">
-                            <i className="fas fa-phone"></i>
+                            <i className="fa fa-phone"></i>
                         </div>
                         <div className="col-11">
                             <span className=""> +91-44-24500792</span>
@@ -36,7 +38,7 @@ const Footer = () => {
                     </div>
                     <div className="row">
                         <div className="col-1">
-                            <i className="fas fa-envelope"></i>
+                            <i className="fa fa-envelope"></i>
                         </div>
                         <div className="col-11">
                             <span className=""> cse@stjosephs.ac.in</span>
@@ -44,13 +46,15 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="row p-4 copyright">
-                <div className="text-center ">
-                    <i className="far fa-copyright"></i>
+          
+            <div className="row pt-3 copyright">
+                <div className="text-center">
+                    <i className="fa fa-copyright"></i>
                     <span> 2021 All rights reserved by St. Joseph's College Of Engineering</span>
                     <span className="d-block mt-2 credits"> Maintained by the Department of Computer Science and Engineering</span>
                 </div>
             </div>
+
         </div>
     )
 }
--- cs_site_react/src/components/visionAndMission.jsx
+++ cs_site_react/src/components/visionAndMission.jsx
@@ -1,13 +1,37 @@
+import { eventListeners } from "@popperjs/core";
 import React from "react";
 
 const VisionAndMission = () => {
 
+    function openCity(evt, cityName) {
+        alert("hello")
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the button that opened the tab
+      //   document.getElementById(cityName).className += " transition";
+        document.getElementById(cityName).style.display = "block";
+        
+        evt.currentTarget.className += " active";
+    }
     return (
         
-        <div class="banner-wrapper">
+        <div class="banner-wrapper">    
         
             <div class="tab align-center">
-                <button class="tablinks active" onclick="openCity(event, 'visionAndMission')">Vision & Mission</button>
+                <button class="tablinks active" onclick={() => openCity('visionAndMission')}>Vision & Mission</button>
                 <button class="tablinks" onclick="openCity(event, 'PEOPSO')">PEO & PSO</button>
                 <button class="tablinks" onclick="openCity(event, 'programOutcome')">Program Outcome</button>
                 <button class="tablinks" onclick="openCity(event, 'courseOutcome')">Course Outcome</button>
