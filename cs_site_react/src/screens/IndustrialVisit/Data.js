b690028a60857b6f2eb02620c87719763bee12c6
Hemanth <hemanthhari2000@gmail.com>
2021-09-17 14:41:48 +0530

    [Hemanth] Add Industrial Visit Page

---
+++ cs_site_react/src/screens/IndustrialVisit/Data.js
@@ -0,0 +1,26 @@
+export const industrialVisitData = [
+    {
+        "class": "II CSE 'A'",
+        "industry" : "NLC, Neyveli",
+        "date": "4/2/2019",
+        "noStudents": "57/61"
+    },
+    {
+        "class": "II CSE 'A'",
+        "industry" : "NLC, Neyveli",
+        "date": "4/2/2019",
+        "noStudents": "57/61"
+    },
+    {
+        "class": "II CSE 'A'",
+        "industry" : "NLC, Neyveli",
+        "date": "4/2/2019",
+        "noStudents": "57/61"
+    },
+    {
+        "class": "II CSE 'A'",
+        "industry" : "NLC, Neyveli",
+        "date": "4/2/2019",
+        "noStudents": "57/61"
+    },
+];
