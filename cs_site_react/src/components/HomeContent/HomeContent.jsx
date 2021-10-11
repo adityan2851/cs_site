172b2b232bf4c84df6465844fb34dbdcc280a3b1
Hemanth <hemanthhari2000@gmail.com>
2021-10-11 09:51:58 +0530

    [Hemanth] Add YT video

--- cs_site_react/src/components/HomeContent/HomeContent.jsx
+++ cs_site_react/src/components/HomeContent/HomeContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import HomeCarousel from "../HomeCarousel/HomeCarousel";
 
-import Banner from "../Banner"
+import Banner from "../Banner";
 
 import "./HomeContent.css";
 
@@ -20,8 +20,26 @@ const HomeContent = ({ content }) => {
         </Row>
         <hr />
         <Row>
-          <Col md>
+          <Col md className="d-flex flex-column align-items-center">
             <h1 className="text-wrapper">Watch our Testimonials</h1>
+            {/* <Container md className="text-center video-frame">
+              <iframe
+                src="https://www.youtube.com/embed/0Mh8KPg1xNU?autoplay=0&mute=1&loop=1&playlist=0Mh8KPg1xNU"
+                frameborder="0"
+                width="100%"
+                height="500"
+                title="Youtube-Video"
+              ></iframe>
+            </Container> */}
+            <div className=" md video-wrapper">
+              <iframe
+                title="youtube title"
+                src="https://www.youtube.com/embed/0Mh8KPg1xNU?autoplay=0&mute=1&loop=1&playlist=0Mh8KPg1xNU"
+                frameBorder="0"
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
           </Col>
           <Banner />
         </Row>
