630c7d71a8ac2da7d45ad1e6f39069ab3d73f06b
Adityan B <adhi2851@gmail.com>
2021-10-12 12:52:22 +0530

    [Adityan] Homepage SVG update

--- cs_site_react/src/App.js
+++ cs_site_react/src/App.js
@@ -49,6 +49,8 @@ function App() {
             exact
             component={ProfessionalActivity}
           />
+          <Route path="/page-error" exact component={ErrorPage404}/>
+          <Redirect to="/page-error"/>
         </Switch>
       </Router>
       <Footer />
