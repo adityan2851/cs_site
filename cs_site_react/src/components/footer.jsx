be7af89048d8ad6f3417307d9fad5ecc6ee083a3
Hilda24 <godlin.hilda@gmail.com>
2021-10-12 10:21:07 +0530

    Footer Added

--- cs_site_react/src/components/footer.jsx
+++ cs_site_react/src/components/footer.jsx
@@ -1,24 +1,67 @@
 import React from 'react'
 import background from "../images/Footer.svg"
-// import 'font-awesome/css/font-awesome.min.css';
+import { useState, useEffect } from 'react';
+import {Collapse} from 'react-bootstrap'
+import 'font-awesome/css/font-awesome.min.css';
+
 
 const Footer = () => {
+    var open = true;
+    var [openAbout, setAbout] = useState(true);
+    var [openResources, setResources] = useState(true);
+    const [dropDownStyle, setDropDownStyle] = useState({display: "none"});
+    
+
+    useEffect(() => {
+        if(window.innerWidth <600){
+            setAbout(!openAbout);
+            setResources(!openResources)
+            setDropDownStyle({display: "block"});
+        }
+      },[]);
+
+      
+
+   
+    
     return (
-        <div className="footer d-flex container-fluid" style={{ backgroundImage: `url(${background})` }}>
-            <div className="row mt-3 pt-5 footer-contents">
-                <div className="col-3">
-                    <span className="fw-bold"> ABOUT</span>
-                    <span className="d-block mt-3 mb-2 details"> Faculty</span>
-                    <span className="d-block mb-2 details"> Mentor</span>
-                    <span className="d-block mb-2 details"> Student Welfare Committee</span>
+        <div className="footer d-flex container-fluid">
+            <div className="row pt-5 footer-contents">
+                <div className="col-md-3">
+                <button className="footer-about"
+        onClick={() => {
+            setAbout(!openAbout)}}
+        aria-controls="example-collapse-text"
+        aria-expanded={openAbout}>
+        <span className="fw-bold"> ABOUT</span>
+        <i class="fa fa-caret-down dropdown" style={dropDownStyle} aria-hidden="true"></i>
+           </button>
+                <Collapse in={openAbout}>
+                <ul className="footer-list">
+                    <li className="d-block mt-3 mb-2 details"> Faculty</li>
+                    <li className="d-block mb-2 details"> Mentor</li>
+                    <li className="d-block mb-2 details"> Student Welfare Committee</li>
+                </ul>
+                </Collapse>
                 </div>
-                <div className="col-3">
-                    <span className="fw-bold"> RESOURCES</span>
-                    <span className="d-block mt-3 mb-2 details">News letter</span>
-                    <span className="d-block mb-2 details">Magazine</span>
-                    <span className="d-block mb-2 details">IEEE & CSI</span>
+                <div className="col-md-3">
+                <button className="footer-about"
+        onClick={() => { console.log(window.innerWidth);
+            setResources(!openResources)}}
+        aria-controls="example-collapse-text"
+        aria-expanded={openResources}>
+        <span className="fw-bold"> RESOURCES</span>
+        <i class="fa fa-caret-down dropdown"  style={dropDownStyle} aria-hidden="true"></i>
+           </button>
+                <Collapse in={openResources}>
+                <ul className="footer-list">
+                    <li className="d-block mt-3 mb-2 details"> News Letter</li>
+                    <li className="d-block mb-2 details"> Magazine</li>
+                    <li className="d-block mb-2 details"> IEEE & CSI</li>
+                </ul>
+                </Collapse>
                 </div>
-                <div className="col-4">
+                <div className="col-md-3">
                     <span className="fw-bold"> CONTACT INFORMATION</span>
                     <div className="row mt-3 mb-2">
                         <div className="col-1">
@@ -30,7 +73,7 @@ const Footer = () => {
                     </div>
                     <div className="row mb-2">
                         <div className="col-1">
-                            <i className="fa fa-phone"></i>
+                            <i className="fa fa-phone d-inline"></i>
                         </div>
                         <div className="col-11">
                             <span className=""> +91-44-24500792</span>
@@ -54,8 +97,8 @@ const Footer = () => {
                     <span className="d-block mt-2 credits"> Maintained by the Department of Computer Science and Engineering</span>
                 </div>
             </div>
-
         </div>
+    
     )
 }
 
---
+++ cs_site_react/src/screens/Patents/Patents.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { PatentsData } from "./Data";
+import { Table } from "react-bootstrap";
+
+const Patents = () => {
+  return (
+    <div>
+      <div className="container">
+        <br />
+        <h2 style={{ textAlign: "center", fontWeight: "bold" }}>Patents</h2>
+
+        {PatentsData.map((obj, i) => (
+          <div>
+            <br />
+
+            <Table bordered responsive="md" hover>
+              <thead>
+                <tr>
+                  <th scope="col">S.No</th>
+                  <th scope="col">Year</th>
+                  <th scope="col">CBR Number</th>
+                  <th scope="col">Application Number</th>
+                  <th scope="col">Faculty Name</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {obj["data"].map((entry, index) => (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{entry["year"]}</td>
+                    <td>{entry["cbrnumber"]}</td>
+                    <td>{entry["appnumber"]}</td>
+                    {console.log(entry["faculty"], "world")}
+                    <td>{entry["faculty"].map((name,j) => (
+                        <span>{name}<br/></span>
+                    ))}</td>
+                    <td>{entry["title"]}</td>
+                    <td>{entry["status"]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Patents;
