34c09c7d6d8355b112d594fd44e6ba97fbc5f450
Hilda24 <godlin.hilda@gmail.com>
2021-10-22 14:29:21 +0530

    Add Mentors

---
+++ cs_site_react/src/screens/Mentors/Data.js
@@ -0,0 +1,154 @@
+export const mentorData = [
+    {
+        chairPerson : "Dr.A. CHANDRASEKAR",
+        batch : "2017-2021",
+        staff : [
+            {
+                name : "Dr A Sheryl Oliver", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr Jesline",
+                designation : "Associate Professor",
+            },
+            {
+                name : "Mr R Ranjith",
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Dr S Jothi",
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr M Anuradha",
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr J Jean Justus",
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr M P Rajakumar",
+                designation : "Professor",
+            },
+            {
+                name : "Ms S Shanthini",
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Mr S Vinu",
+                designation : "Assistant Professor",
+            },
+        ]
+    },
+    {
+        chairPerson : "Dr.G. MARIAKALAVATHY",
+        batch : "2019-2023",
+        staff : [
+            {
+                name : "Dr G Murugesan", 
+                designation : "Professor",
+            },
+            {
+                name : "Ms P N Jeipratha", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Ms S Janani", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Dr. B. Uma Maheswari", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr D Rosy Salomi Victoria", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Mr N Manikandan", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Dr R Hemalatha", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Mr K Balaji", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Ms Jenif D Souza WS",
+                designation : "Assistant Professor"
+            }
+        ]
+    },
+    {
+        chairPerson : "Dr.R. PUGALENTHI",
+        batch : "2018-2022",
+        staff : [
+            {
+                name : "Dr J T Anita Rose", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr F Sangeetha Francelin Vinnarasi", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr J Ramya", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Dr V Anjanadevi", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Mr P Varun", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Mr A Prabhu Chakkaravarthy", 
+                designation : "Assistant Professor",
+            },
+            {
+                name : "Dr N Angel", 
+                designation : "Associate Professor",
+            },
+            {
+                name : "Ms K Sudha", 
+                designation : "Associate Professor",
+            },
+
+        ]
+    },
+    {
+        chairPerson : "Dr.A. CHANDRASEKAR",
+        batch : "M.E. Ph.D",
+        staff : [
+            {
+                name : "Dr N Mythili", 
+                designation : "Associate Professor",
+            }
+        ]
+    },
+    {
+        staff : [
+            {
+                name : "Dr M P Rajakumar", 
+                designation : "Professor",
+                head : "Exams"
+            },
+            {
+                name : "Dr. B. Diwan", 
+                designation : "Associate Professor",
+                head : "Placements"
+            },
+            {
+                name : "Mr.K. Rajaganesh", 
+                designation : "Assistant Professor",
+                head : "Hostel"
+            },
+        ]
+    },
+]
