0b1ca3e82a0f5ab5a1184c53434ed908a13197a8
Hemanth <hemanthhari2000@gmail.com>
2021-10-23 13:53:50 +0530

    [Hemanth] Modify Alumni PG detail on hover effect

--- cs_site_react/src/screens/Alumni/AlumniCard.jsx
+++ cs_site_react/src/screens/Alumni/AlumniCard.jsx
@@ -1,48 +1,61 @@
-
 import { Accordion, Card } from "react-bootstrap";
+import "./Alumni.css";
 
-const AlumniCard = ({image, designation, year, place, pg, alumniName, testimonial}) => {
-    return (
-        <div class="card mb-4 Box shadow">
-          <div class="card-body text-center">
-            <ul class="list-unstyled display-block mx-auto">
 
-                <h4 className="alumni-name">{alumniName}</h4>
-                <b><p className = "staff-name">{year}</p></b>
-                {/* <img src="" alt="profile" className = "card-img-top contributor-img"/> */}
-              <img alt="profile" class="card-img-top contributor-img" src={image} />
-              {/* <div className = "accordion accordion-flush" ></div> */}
-              {(pg) && (<Accordion defaultActiveKey = "1" flush>
-                  <Card>
-                      <Accordion.Toggle as = {Card.Header} eventKey = "0"> PG Details </Accordion.Toggle>
-                      <Accordion.Collapse eventKey = "0"> 
-                      <Card.Body> 
-                          <span>{pg['course']} <br/>
-                          {pg['university']} <br/>
-                          {pg['year']}
-                              </span>
-                              </Card.Body></Accordion.Collapse>
-                  </Card>
-                </Accordion>) }
-              
-                
-
-    
-              <li style={{ marginTop: "20px" }}>
-                <b>
-                  <span class="alumni-designation">{designation}</span>{" "}
-                </b>
-                <p style={{ opacity: "80%" }}>
+const AlumniCard = ({
+  image,
+  designation,
+  year,
+  place,
+  pg,
+  alumniName,
+  testimonial,
+}) => {
+  return (
+    <div class="card mb-4 Box shadow">
+      <div class="card-body text-center">
+        <ul class="list-unstyled display-block mx-auto">
+          <h4 className="alumni-name">{alumniName}</h4>
+          <b>
+            <p className="staff-name">{year}</p>
+          </b>
+          {/* <img src="" alt="profile" className = "card-img-top contributor-img"/> */}
+          <img alt="profile" class="card-img-top contributor-img alumini-image" src={image} />
+          {/* <div className = "accordion accordion-flush" ></div> */}
+          {pg && (
+            <Accordion
+              className="accordion-dropdown"
+              defaultActiveKey="1"
+              flush
+            >
+              <Card className="accordion-card">
+                <Accordion.Toggle as={Card.Header} eventKey="0">
                   {" "}
-                  {place}
-                </p>
-              </li>
-            </ul>
-    
-           
-          </div>
-        </div>
-      );
+                  PG Details{" "}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <span>
+                      {pg["course"]} <br />
+                      {pg["university"]} <br />
+                      {pg["year"]}
+                    </span>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          )}
+
+          <li style={{ marginTop: "20px" }}>
+            <b>
+              <span class="alumni-designation">{designation}</span>{" "}
+            </b>
+            <p style={{ opacity: "80%" }}> {place}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AlumniCard;
