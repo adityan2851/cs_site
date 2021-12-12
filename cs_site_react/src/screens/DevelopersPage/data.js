ec091ea79f7a697014980c0080e86e7ef42f4d43
Fabian Christopher <54461744+FabChris01@users.noreply.github.com>
2021-12-12 12:29:13 +0530

    [Fab] Added Images

--- cs_site_react/src/screens/DevelopersPage/data.js
+++ cs_site_react/src/screens/DevelopersPage/data.js
@@ -5,35 +5,41 @@
 // import sharmi from "../../assets/images/developers/sharmi-img.png";
 // import goblin from "../../assets/images/developers/goblin-img.png";
 import ph from "../../assets/images/developers/placeholder.jpg";
+import fab from "../../assets/DevAssets/4043.JPG";
+import amonth from "../../assets/DevAssets/4059.JPG";
+import adi from "../../assets/DevAssets/4009.JPG";
+import jeni from "../../assets/DevAssets/18CS150.JPG";
+import sharmi from "../../assets/DevAssets/18CS215.JPG";
+import godlin from "../../assets/DevAssets/18CS225.JPG";
 
 const dev_details = [
     [{
-        image: ph,
+        image: adi,
         devName: "Adityan B",
         linkedinLink: "https://www.linkedin.com/in/adityan-b-6b2a35183/"
     },
     {
-        image: ph,
+        image: fab,
         devName: "Fabian Christopher",
         linkedinLink: "https://www.linkedin.com/in/fabchris10/" 
     },
     {
-        image: ph,
+        image: amonth,
         devName: "Hemanth Kumar H",
         linkedinLink: "https://www.linkedin.com/in/hemanthhari2000/"
     }],
     [{
-        image: ph,
+        image: godlin,
         devName: "Godlin Hilda",
         linkedinLink: "https://www.linkedin.com/in/godlin-hilda/"
     },
     {
-        image: ph,
+        image: sharmi,
         devName: "Sharmila S",
         linkedinLink: "https://www.linkedin.com/in/sharmila-s22/"
     },
     {
-        image: ph,
+        image: jeni,
         devName: "Jenisha Roshan",
         linkedinLink: "https://www.linkedin.com/in/jenisha-roshan/"
     },],
